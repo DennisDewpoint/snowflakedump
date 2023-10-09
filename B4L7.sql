799a5ca09e9784dadabe298b08ec9b10bd5ed7db
DennisDewpoint <123739252+DennisDewpoint@users.noreply.github.com>
2023-10-09 13:59:24 +0200

    Update B4L7.sql

--- B4L7.sql
+++ B4L7.sql
@@ -19,5 +19,27 @@ group by trail_name;
 
 select
 feature_name
-,st_length( ( )) as trail_length
+,st_length(to_geography(geometry)) as trail_length
 from denver_area_trails;
+
+select get_ddl('view', 'DENVER_AREA_TRAILS');
+
+create or replace view DENVER_AREA_TRAILS(
+    FEATURE_NAME,
+    FEATURE_COORDINATES,
+    GEOMETRY,
+    TRAIL_LENGTH,
+    FEATURE_PROPERTIES,
+    SPECS,
+    WHOLE_OBJECT
+) as
+select
+$1:features[0]:properties:Name::string as feature_name
+,$1:features[0]:geometry:coordinates::string as feature_coordinates
+,$1:features[0]:geometry::string as geometry
+,st_length(to_geography(geometry)) as trail_length
+,$1:features[0]:properties::string as feature_properties
+,$1:crs:properties:name::string as specs
+,$1 as whole_object from @trails_geojson (file_format => ff_json);
+
+select * from DENVER_AREA_TRAILS;
